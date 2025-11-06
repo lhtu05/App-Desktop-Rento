70301fc3f25eb48f6bfa24c0b9d0423e033032f6
zef <lehuynhtu05@gmail.com>
2025-11-06 17:03:13 +0700

    Update MainPage

--- main/App.xaml.cs
+++ main/App.xaml.cs
@@ -9,12 +9,7 @@ namespace main
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            var mainWindow = new MainWindow();
-            mainWindow.Show();
-        }
+        
     }
 
 }
